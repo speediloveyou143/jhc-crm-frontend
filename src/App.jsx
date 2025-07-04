0f95a9ca1df27ae5dd5348f45df00f13e4ecab22
prasadyadav772723 <prasadyadav772723@gmail.com>
2025-07-04 11:05:31 +0530

    'feature-pricing'

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AppLayout from "./layouts/AppLayout";
 import Signin from "./pages/Signin";
 import Signup from "./pages/Signup";
+import Pricing from "./pages/pricingpage/Pricing";
 
 export default function App() {
   return (
@@ -10,6 +11,8 @@ export default function App() {
         <Route path="/" element={<AppLayout />}>
           <Route path="signin" element={<Signin />} />
           <Route path="signup" element={<Signup />} />
+          <Route path="pricing" element={< Pricing/>} >
+          </Route>
         </Route>
       </Routes>
     </Router>
